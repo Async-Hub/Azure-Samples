e8075e09341a24b2f2a9abcc503e522f23959f68
Karen Tazayan <karen.tazayan@outlook.com>
2020-07-21 16:37:12 +0400

    New "Blobs" project for "Azure-Storage".

---
+++ Azure-Storage/Blobs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+
+namespace Blobs
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            const string connectionString = "Connection string";
+            var blobServiceClient = new BlobServiceClient(connectionString);
+            var response = await blobServiceClient.CreateBlobContainerAsync("sample-container");
+            var blobContainerClient = response.Value;
+
+            Console.WriteLine(blobContainerClient.Name);
+        }
+    }
+}
